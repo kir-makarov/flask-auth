b673f60fd0792e20f5fa2b440a62f6ea0c2a207a
balyakin <92801118+balyakin@users.noreply.github.com>
2022-05-24 16:45:38 +0300

    added three tests for /login endpoint

--- tests/functional/test_api_login.py
+++ tests/functional/test_api_login.py
@@ -1,6 +1,7 @@
 import http
 
-def test_api_login(client):
+
+def test_api_login_siccessful(client):
     test_client = client.test_client()
     response = test_client.post(
         path="/v1/register",
@@ -25,3 +26,54 @@ def test_api_login(client):
     result = response.json
     assert "access_token" in result
     assert "refresh_token" in result
+
+
+def test_api_login_wrong_password(client):
+    test_client = client.test_client()
+    response = test_client.post(
+        path="/v1/register",
+        json={
+            "username": "test",
+            "password": "test",
+        },
+    )
+
+    assert response.status_code == http.HTTPStatus.CREATED
+
+    response = test_client.post(
+        path="/v1/login",
+        json={
+            "username": "test",
+            "password": "wrong_password",
+        },
+    )
+
+    assert response.status_code == http.HTTPStatus.UNAUTHORIZED
+
+    result = response.json
+    assert result == {"message": "Invalid credentials"}
+
+def test_api_login_unknown_user(client):
+    test_client = client.test_client()
+    response = test_client.post(
+        path="/v1/register",
+        json={
+            "username": "test",
+            "password": "test",
+        },
+    )
+
+    assert response.status_code == http.HTTPStatus.CREATED
+
+    response = test_client.post(
+        path="/v1/login",
+        json={
+            "username": "unknown_username",
+            "password": "the_password",
+        },
+    )
+
+    assert response.status_code == http.HTTPStatus.UNAUTHORIZED
+
+    result = response.json
+    assert result == {"message": "Invalid credentials"}
