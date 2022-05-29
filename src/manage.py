c9a48d1af2adea0551a3088a7134f0efb0f46d7d
T23 <petr777@yahoo.com>
2022-05-29 21:01:34 +0300

    manage.py

--- src/manage.py
+++ src/manage.py
@@ -1,27 +1,45 @@
+import uuid
+
 import click
-from models.user import UserModel, Access
+from models.user import UserModel, RoleModel
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from core.config import settings
-
+from uuid import uuid4
 
 engine = create_engine(settings.postgres.uri)
 session = Session(bind=engine)
 
 
-@click.command()
-@click.option('--username', prompt=True, help='Name of the password.')
-@click.option('--password', prompt=True, help='Name of the password.')
+
+# @click.command()
+# @click.option('--username', prompt=True, help='Name of the password.')
+# @click.option('--password', prompt=True, help='Name of the password.')
 def hello(username, password):
-    try:
-        admin = UserModel(username=username, password=UserModel.generate_hash(password))
-        admin.access = Access.admin
-        session.add(admin)
-        session.commit()
-        click.echo(f'admin created successfully.')
-    except Exception as err:
-        click.echo(f'admin created {err}')
+
+    user = session.query(UserModel).filter(UserModel.username == username).first()
+    if user:
+        click.echo(f'the name is already taken')
+
+    role = session.query(RoleModel).filter(RoleModel.name == 'admin').first()
+    if not role:
+        role = RoleModel(name='admin')
+
+    user = UserModel(username=username, password=UserModel.generate_hash(password))
+    print(user.id, role.id)
+
+    # session.add(admin)
+    # session.commit()
+
+
+    #     # admin = UserModel(username=username, password=UserModel.generate_hash(password))
+    #     #
+    #     # session.add(admin)
+    #     # session.commit()
+    #     # click.echo(f'admin created successfully.')
+    # except Exception as err:
+    #     click.echo(f'admin created {err}')
 
 
 if __name__ == '__main__':
-    hello()
+    hello(username='petr', password='222')
